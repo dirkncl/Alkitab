6e4f374feaa94a540d8605ae150441973eca20fb
Dirk Levinus Nicolaas <dirkncl@gmail.com>
2020-10-30 15:16:34 +0800

    Add files via upload

---
+++ js/getVerses.js
@@ -0,0 +1,276 @@
+﻿/*************************************/
+/*     Dirk L. Nicolaas              */
+/*    Masih dalam tahap proses       */ 
+/*  belajar dari yang sederhana      */
+/*   Sawangan - Minahasa Utara       */
+/*  Sulawesi Utara - Indonesia       */
+/*      15 November 2018             */
+/*************************************/
+/* ********************************************* */
+
+var ref_ayat ="";
+var bible_version;
+var InputString = decodeURIComponent(location.search).replace(/\+/g, " ").replace('?','');
+var inputs;
+if(InputString.includes("&")){
+  inputs = InputString.split("&")
+  InputString = inputs[0]
+}
+
+if(InputString.includes("[")){
+  InputString = InputString.split("[")
+  ref_ayat = InputString[0].trim()
+  bible_version = InputString[1].replace("]","")
+}
+else if(InputString.includes("(")){
+  InputString = InputString.split("(")
+  ref_ayat = InputString[0].trim()
+  bible_version = InputString[1].replace(")","")
+}
+else if(InputString.includes("--")){
+  InputString = InputString.split("--")
+  ref_ayat = InputString[0].trim()
+  bible_version = InputString[1].trim()
+}
+else{
+    ref_ayat = InputString
+    bible_version = ""
+}  
+window.onload= function(){
+  alkitab(ref_ayat,bible_version)
+  bg()
+}
+
+function alkitab(verse_refrence, version){
+  version = version||"TB"
+  var ver;
+  var alkitabDB;
+  alkitabDB={
+    id_TB:"data/alkitab-tb",
+    en_kjv:"data/bible-kjv",
+    en_akjv:"data/bible-akjv"
+  };
+  var versionName
+  switch (version.toUpperCase()){
+    case "": 
+    case "TB-LAI":
+    case "TB": ver = alkitabDB.id_TB;   versionName = "Alkitab (TB - LAI)";break;
+    case "AV":
+    case "KJV":ver = alkitabDB.en_kjv; versionName = "Bible (KJV - 1769)";break;
+    case "A-KJV":
+    case "AKJV":ver = alkitabDB.en_akjv; versionName = "Bible (American - KJV)";break;
+  }
+  
+  var alkitabArray=[]; 
+  function loadingKitab(alk){
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", alk,false);
+    xhr.onload = parse;
+    xhr.send();
+  };
+  function parse() {
+      alkitabArray = this.response;
+      alkitabArray.trim();
+      alkitabArray = alkitabArray.split('\n').join('\n');
+      alkitabArray = alkitabArray.split('\n');
+    
+  };
+ 
+  function getAyatAlkitab(referensi){
+    var referensi_diminta = referensi;
+    var referensi_jawab = "";
+    var result = [];
+    var tmp;
+    for (var i = 0; i< alkitabArray.length; i++){
+      tmp = alkitabArray[i];
+      result[0] = tmp.split('^')[0];
+      result[1] = tmp.split('^')[1];
+      if(referensi_diminta === result[0]) {
+        referensi_jawab = result[1]
+      };
+    } 
+    
+    return referensi_jawab;
+  };
+
+  var ayatToDisplay = document.getElementById('ayatToDisplay');
+  var content = document.getElementById('content');
+  content.innerHTML = "";
+  var Kitab_Pasal_Ayat = "";
+  var hasil = [];
+  var result = "";
+  var inputAlkitab;
+  if (verse_refrence !== ""){
+    loadingKitab(ver);
+    document.getElementById('version').className = "blink";
+    document.getElementById('version').innerHTML = versionName;
+    document.getElementById('ref_ayat').innerHTML = verse_refrence+" "+version;
+    //document.title = versionName;
+    document.title = verse_refrence+" "+version;
+    inputAlkitab = verse_refrence
+    var check = toSemiOsis(inputAlkitab).split(".")
+    if(check.length>1){
+      proses(window.event);
+    }
+    else{
+      prosesBook(window.event)
+    }
+    display();
+  }
+
+  function proses(e){
+    e.preventDefault();
+    result = "";
+
+    Kitab_Pasal_Ayat = inputAlkitab;
+    Kitab_Pasal_Ayat = PraParser(Kitab_Pasal_Ayat);
+    
+    console.log(Kitab_Pasal_Ayat)
+    var kitab = Kitab_Pasal_Ayat.kitab,
+        pasal = Kitab_Pasal_Ayat.pasal,
+        ayat = Kitab_Pasal_Ayat.ayat;
+    if (ayat.length>1){
+      for(var i = 0;i<ayat.length;i++){
+        hasil[i] = toSemiOsis(kitab+" "+pasal+":"+ayat[i]+"")
+      }
+      
+    }  
+    else {
+      hasil[0] = toSemiOsis(kitab+" "+pasal+":"+ayat+"")
+    }
+    if(Kitab_Pasal_Ayat=="")ayatToDisplay.style.display = 'none';
+  };
+
+  function prosesBook(e){
+    e.preventDefault();
+    result = "";
+    Kitab_Pasal_Ayat = inputAlkitab;
+    Kitab_Pasal_Ayat = PraParser(Kitab_Pasal_Ayat);
+    var kitab = Kitab_Pasal_Ayat.kitab,
+        pasal = Kitab_Pasal_Ayat.pasal,
+        ayat = Kitab_Pasal_Ayat.ayat;
+    var pas=0;
+    for(var i = 0;i<ayat.length;i++){
+       if(ayat[i]==="1") pas +=1;
+       //console.log(kitab[0]+" "+pas+":"+ayat[i])
+        hasil[i] = toSemiOsis(kitab[0]+" "+pas+":"+ayat[i]+"")
+    }
+    if(Kitab_Pasal_Ayat=="")ayatToDisplay.style.display = 'none';
+  };
+
+  function display(){
+    content.innerHTML = "";
+    ayatToDisplay.style.display='block';
+    content.style.textAlign='left';
+    var sup,sup2;
+    if(hasil.length>1){
+      for(var i=0;i<hasil.length;i++){
+        var osisSplit = hasil[i].split(".");
+        sup = osisSplit.pop();
+        sup2 = osisSplit.pop();
+        if(sup==="1") {
+          //sup = '<font size="6" color="ivory">'+sup2+'</font>';//+":"+sup
+          sup = '<span class="blink" style="opacity:0.8;color:ivory;float:left;font-size:2.2em;padding: 5px 20 5px 40px;margin-right: 9px;">'+sup2+'</span>';
+          //sup = '<span style="float:left;padding: 10px 20 10 40px;margin-right: 9px;"><font size="6" color="ivory">'+sup2+'</font></span>';
+          sup = "<span>"+sup+"</span>&nbsp;"
+          addbreak = '<br><br><br><br>'
+          
+        }else{
+          sup = sup.sup()
+          //sup = "<span style='color:'"+supColor+"';font-size:'"+supFont+"'em;opacity:'"+supOpa+"'>"+sup+"</span>&nbsp;"
+          sup = "<span style='opacity:0.8'>"+sup+"</span>&nbsp;"
+          addbreak = ''
+        };
+
+        if(getAyatAlkitab(hasil[i])!==""){
+          result += sup + getAyatAlkitab(hasil[i])+"<br><br>";
+        }
+      }
+      
+      content.innerHTML=addbreak+"<br>"+result+"<br><br>";
+    }  
+    else{
+      sup = hasil[0].split(".").pop();
+      sup = sup.sup()
+      sup = "<span  style='opacity:0.8'>"+sup+"</span>&nbsp;"
+      //"<span style='font-size:0.8em;opacity:0.7'>"+sup+"</span>&nbsp;"
+      //console.log(sup)
+      result = sup + getAyatAlkitab(hasil[0]);
+      content.innerHTML="<br>"+result+"<br><br>";
+    }
+  };
+  
+  function clear(){
+    for(var i=0;i<hasil.length;i++){
+      hasil[i] = "";
+    };
+    content.innerHTML = result = ""
+  };
+ 
+  document.addEventListener("keydown",function(e){
+    if(13 === e.keyCode||!(e.ctrlKey||e.metaKey))
+      switch(e.keyCode){
+        case 13:display();break;//[enter]
+        case 27:clear();break;  //[Esc]
+      }
+  });
+};
+
+function bg()  {
+  var body = document.body;
+  
+  var backgrounds = [ 
+    "imgs/air-kehidupan-by-rumi-source_assets-a2_kompasiana_com.jpg",
+    "imgs/alkitab_source_assets-a2_kompasiana_com.jpg"
+   ]
+  
+  var current = parseInt(Math.random()*backgrounds.length)%backgrounds.length;
+  function nextBackground() {
+    body.background = backgrounds[parseInt(Math.random()*backgrounds.length)%backgrounds.length];
+    body.background.repeat = 'no-repeat';
+    body.background.size = 'cover';
+    setTimeout(nextBackground, 10000);
+  }
+  setTimeout(nextBackground, 10000);
+  body.background = backgrounds[current];
+  body.background.repeat = 'no-repeat';
+  body.background.size = 'cover';
+}
+
+var scrollbarAndBlink = `
+  ::-webkit-scrollbar {
+      height: 8px;
+      width: 8px;
+      background: #104410;
+      border-radius: 10px;
+  }
+  
+  ::-webkit-scrollbar-thumb {
+      background: #106610;
+      border-radius: 4px;
+  }
+  
+  ::-webkit-scrollbar-corner {
+      background: #000000;
+  }
+  @keyframes blink {  
+    0% { color: gold; }
+    25% { color: ivory; }
+    50% { color: lightgray; }
+    75% { color: gray; }
+    100% { color: black; }
+  }
+  @-webkit-keyframes blink {
+    0% { color: gold; }
+    25% { color: ivory; }
+    50% { color: lightgray; }
+    75% { color: gray; }
+    100% { color: black; }
+  }
+  .blink {
+    -webkit-animation: blink 3s linear infinite;
+    -moz-animation: blink 3s linear infinite;
+    animation: blink 3s linear infinite;
+  }
+`;
+(sty=document.createElement("style")).textContent=scrollbarAndBlink,document.head.appendChild(sty)
---
+++ js/refParser.js
@@ -0,0 +1,345 @@
+/*************************************/
+  /*     Dirk L. Nicolaas              */
+  /*    Masih dalam tahap proses       */ 
+  /*  belajar dari yang sederhana      */
+  /*   Sawangan - Minahasa Utara       */
+  /*  Sulawesi Utara - Indonesia       */
+  /*      15 November 2018             */
+  /*************************************/
+      
+  var BCV = {
+    "Gen":[31,25,24,26,32,22,24,22,29,32,32,20,18,24,21,16,27,33,38,18,34,24,20,67,34,35,46,22,35,43,55,32,20,31,29,43,36,30,23,23,57,38,34,34,28,34,31,22,33,26],
+    "Exod":[22,25,22,31,23,30,25,32,35,29,10,51,22,31,27,36,16,27,25,26,36,31,33,18,40,37,21,43,46,38,18,35,23,35,35,38,29,31,43,38],
+    "Lev":[17,16,17,35,19,30,38,36,24,20,47,8,59,57,33,34,16,30,37,27,24,33,44,23,55,46,34],
+    "Num":[54,34,51,49,31,27,89,26,23,36,35,16,33,45,41,50,13,32,22,29,35,41,30,25,18,65,23,31,40,16,54,42,56,29,34,13],
+    "Deut":[46,37,29,49,33,25,26,20,29,22,32,32,18,29,23,22,20,22,21,20,23,30,25,22,19,19,26,68,29,20,30,52,29,12],
+    "Josh":[18,24,17,24,15,27,26,35,27,43,23,24,33,15,63,10,18,28,51,9,45,34,16,33],
+    "Judg":[36,23,31,24,31,40,25,35,57,18,40,15,25,20,20,31,13,31,30,48,25],
+    "Ruth":[22,23,18,22],
+    "1Sam":[28,36,21,22,12,21,17,22,27,27,15,25,23,52,35,23,58,30,24,42,15,23,29,22,44,25,12,25,11,31,13],
+    "2Sam":[27,32,39,12,25,23,29,18,13,19,27,31,39,33,37,23,29,33,43,26,22,51,39,25],
+    "1Kgs":[53,46,28,34,18,38,51,66,28,29,43,33,34,31,34,34,24,46,21,43,29,53],
+    "2Kgs":[18,25,27,44,27,33,20,29,37,36,21,21,25,29,38,20,41,37,37,21,26,20,37,20,30],
+    "1Chr":[54,55,24,43,26,81,40,40,44,14,47,40,14,17,29,43,27,17,19,8,30,19,32,31,31,32,34,21,30],
+    "2Chr":[17,18,17,22,14,42,22,18,31,19,23,16,22,15,19,14,19,34,11,37,20,12,21,27,28,23,9,27,36,27,21,33,25,33,27,23],
+    "Ezra":[11,70,13,24,17,22,28,36,15,44],
+    "Neh":[11,20,32,23,19,19,73,18,38,39,36,47,31],
+    "Esth":[22,23,15,17,14,14,10,17,32,3],
+    "Job":[22,13,26,21,27,30,21,22,35,22,20,25,28,22,35,22,16,21,29,29,34,30,17,25,6,14,23,28,25,31,40,22,33,37,16,33,24,41,30,24,34,17],
+    "Ps":[6,12,8,8,12,10,17,9,20,18,7,8,6,7,5,11,15,50,14,9,13,31,6,10,22,12,14,9,11,12,24,11,22,22,28,12,40,22,13,17,13,11,5,26,17,11,9,14,20,23,19,9,6,7,23,13,11,11,17,12,8,12,11,10,13,20,7,35,36,5,24,20,28,23,10,12,20,72,13,19,16,8,18,12,13,17,7,18,52,17,16,15,5,23,11,13,12,9,9,5,8,28,22,35,45,48,43,13,31,7,10,10,9,8,18,19,2,29,176,7,8,9,4,8,5,6,5,6,8,8,3,18,3,3,21,26,9,8,24,13,10,7,12,15,21,10,20,14,9,6],
+    "Prov":[33,22,35,27,23,35,27,36,18,32,31,28,25,35,33,33,28,24,29,30,31,29,35,34,28,28,27,28,27,33,31],
+    "Eccl":[18,26,22,16,20,12,29,17,18,20,10,14],
+    "Song":[17,17,11,16,16,13,13,14],
+    "Isa":[31,22,26,6,30,13,25,22,21,34,16,6,22,32,9,14,14,7,25,6,17,25,18,23,12,21,13,29,24,33,9,20,24,17,10,22,38,22,8,31,29,25,28,28,25,13,15,22,26,11,23,15,12,17,13,12,21,14,21,22,11,12,19,12,25,24],
+    "Jer":[19,37,25,31,31,30,34,22,26,25,23,17,27,22,21,21,27,23,15,18,14,30,40,10,38,24,22,17,32,24,40,44,26,22,19,32,21,28,18,16,18,22,13,30,5,28,7,47,39,46,64,34],
+    "Lam":[22,22,66,22,22],
+    "Ezek":[28,10,27,17,17,14,27,18,11,22,25,28,23,23,8,63,24,32,14,49,32,31,49,27,17,21,36,26,21,26,18,32,33,31,15,38,28,23,29,49,26,20,27,31,25,24,23,35],
+    "Dan":[21,49,30,37,31,28,28,27,27,21,45,13],
+    "Hos":[11,23,5,19,15,11,16,14,17,15,12,14,16,9],
+    "Joel":[20,32,21],
+    "Amos":[15,16,15,13,27,14,17,14,15],
+    "Obad":[21],
+    "Jonah":[17,10,10,11],
+    "Mic":[16,13,12,13,15,16,20],
+    "Nah":[15,13,19],
+    "Hab":[17,20,19],
+    "Zeph":[18,15,20],
+    "Hag":[15,23],
+    "Zech":[21,13,10,14,11,15,14,23,17,12,17,14,9,21],
+    "Mal":[14,17,18,6],
+  
+    "Matt":[25,23,17,25,48,34,29,34,38,42,30,50,58,36,39,28,27,35,30,34,46,46,39,51,46,75,66,20],
+    "Mark":[45,28,35,41,43,56,37,38,50,52,33,44,37,72,47,20],
+    "Luke":[80,52,38,44,39,49,50,56,62,42,54,59,35,35,32,31,37,43,48,47,38,71,56,53],
+    "John":[51,25,36,54,47,71,53,59,41,42,57,50,38,31,27,33,26,40,42,31,25],
+    "Acts":[26,47,26,37,42,15,60,40,43,48,30,25,52,28,41,40,34,28,41,38,40,30,35,27,27,32,44,31],
+    "Rom":[32,29,31,25,21,23,25,39,33,21,36,21,14,23,33,27],
+    "1Cor":[31,16,23,21,13,20,40,13,27,33,34,31,13,40,58,24],
+    "2Cor":[24,17,18,18,21,18,16,24,15,18,33,21,14],
+    "Gal":[24,21,29,31,26,18],
+    "Eph":[23,22,21,32,33,24],
+    "Phil":[30,30,21,23],
+    "Col":[29,23,25,18],
+    "1Thess":[10,20,13,18,28],
+    "2Thess":[12,17,18],
+    "1Tim":[20,15,16,16,25,21],
+    "2Tim":[18,26,17,22],
+    "Titus":[16,15,15],
+    "Phlm":[25],
+    "Heb":[14,18,19,16,14,20,28,13,28,39,40,29,25],
+    "Jas":[27,26,18,17,20],
+    "1Pet":[25,25,22,19,14],
+    "2Pet":[21,22,18],
+    "1John":[10,29,24,21,21],
+    "2John":[13],
+    "3John":[14],
+    "Jude":[25],
+    "Rev":[20,29,22,11,14,17,17,13,21,11,19,17,18,20,8,21,18,24,21,15,27,20]
+  };    
+  var FilterKitab = [
+  [/^G[en]\D*/i,      "Gen"],[/^K[e]?j\D*/i,      "Gen"],       //Kej.
+  [/^Ex\D*/i,      "Exod"], [/^K[e]?l\D*/i,      "Exod"],       //Kel.
+  [/^L[ev]\D*/i,      "Lev"],[/^I[ma]\D*/i,      "Lev"],        //Ima.
+  [/^Nu\D*/i,      "Num"],[/^Bi\D*/i,      "Num"],              //Bil.
+  [/^D[eu]\D*/i,      "Deut"],[/^U[l]\w*a\D*/i,      "Deut"],   //Ula.
+  [/^J[o]s\D*/i,      "Josh"],[/^Y[o]s\D*/i,      "Josh"],      //Yos.
+  [/^Judg\D*/i,      "Judg"],[/^H?a[k]\D*/i,      "Judg"],      //Hak.
+  [/^R[ut]\D*/i,      "Ruth"],                                  //Rut.
+  [/^1\s*S[am]\D*/i,    "1Sam"],                                //1Sam.
+  [/^2\s*S[am]\D*/i,    "2Sam"],                                //2Sam.
+  [/^1\s*Kg\D*/i,  "1Kgs"],[/^1\s*R\D*/i,  "1Kgs"],             //1Raj.
+  [/^2\s*Kg\D*/i,  "2Kgs"],[/^2\s*R\D*/i,  "2Kgs"],             //2Raj.
+  [/^1\s*C[hr]\D*/i,  "1Chr"],[/^1\s*T[aw]\D*/i,  "1Chr"],      //1Taw.
+  [/^2\s*C[hr]\D*/i,  "2Chr"],[/^2\s*T[aw]\D*/i,  "2Chr"],      //2Taw.
+  [/^E[z]?r\D*/i,      "Ezra"],                                 //Ezr.
+  [/^N[eh]\D*/i,      "Neh"],                                   //Neh.
+  [/^E?st\D*/i,      "Esth"],                                   //Est.
+  [/^J[o]?b\D*/i,      "Job"],[/^Ayb\D*/i,      "Job"],[/^Ayub\D*/i,      "Job"],         //Ayb.
+  [/^P[s]\D*/i,      "Ps"],[/^Ma?z\D*/i,      "Ps"],            //Maz.
+  [/^P[r]?o\D*/i,      "Prov"],[/^A[m]?s\D*/i,      "Prov"],    //Ams.
+  [/^E[cc]\D*/i,      "Eccl"],[/^Pkh\D*/i,      "Eccl"], [/^Pen\D*/i,      "Eccl"],        //Pkh.
+  [/^S[o]n\D*/i,      "Song"],[/^Kid\D*/i,      "Song"],        //Kid.
+  [/^I[s?a]\D*/i,      "Isa"],[/^Ye[s]\D*/i,      "Isa"],       //Yes.
+  [/^J[er]\D*/i,      "Jer"],[/^Ye[r?]\D*/i,      "Jer"],       //Yer.
+  [/^L[am]\D*/i,      "Lam"],[/^R[at]\D*/i,      "Lam"],        //Rat.
+  [/^E[zek]\D*/i,      "Ezek"],[/^Y[e?h]\D*/i,      "Ezek"],    //Yeh.
+  [/^D[a]?n\D*/i,      "Dan"],                                  //Dan.
+  [/^H[os]\D*/i,      "Hos"],                                   //Hos.
+  [/^Joe\D*/i,      "Joel"],[/^Yoe\D*/i,      "Joel"],          //Yoe.
+  [/^Am?o\D*/i,      "Amos"],                                   //Amo.
+  [/^O[ba]\D*/i,      "Obad"],                                  //Oba.
+  [/^Jo?[n]a\D*/i,      "Jonah"],[/^Yu?[n]\D*/i,      "Jonah"], //Yun.
+  [/^Mi\D*/i,      "Mic"],                                      //Mik.
+  [/^Nah?\D*/i,      "Nah"],                                    //Nah.
+  [/^Ha?[b]\D*/i,      "Hab"],                                  //Hab.
+  [/^Ze[p]\D*/i,      "Zeph"],[/^Ze[f]\D*/i,      "Zeph"],      //Zef.
+  [/^H?a[g]\D*/i,      "Hag"],                                  //Hag.
+  [/^Z[e]?c\D*/i,      "Zech"],[/^Z[a]?k\D*/i,      "Zech"],    //Zak.
+  [/^Ma?l\D*/i,      "Mal"],                                    //Mal.
+  [/^Ma?t?t\D*/i,    "Matt"],                                   //Mat.
+  [/^Ma?[rk]\D*/i,  "Mark"],                                    //Mar.
+  [/^L\D*/i,      "Luke"],                                      //Luk.
+  [/^Jo?[hn]\D*/i,  "John"],[/^Yo?[h]\D*/i,  "John"],          //Yoh.
+  [/^Act\D*/i,      "Acts"],[/^Kis*/i,      "Acts"],            //Kis.
+  [/^R[om]\D*/i,    "Rom"],                                     //Rom.
+  [/^1\s*C\D*/i,    "1Cor"],[/^1\s*Kor\D*/i,    "1Cor"],        //1Kor.
+  [/^2\s*C\D*/i,    "2Cor"], [/^2\s*Kor\D*/i,    "2Cor"],       //2Kor.
+  [/^Gal\D*/i,      "Gal"],                                     //Gal.
+  [/^Eph\D*/i,      "Eph"],[/^Efe\D*/i,      "Eph"],            //Efe.
+  [/^P[h]il\D*/i,      "Phil"],[/^Flp\D*/i,      "Phil"],[/^Fil[i]\D*/i,      "Phil"],        //Flp.
+  [/^C\D*/i,      "Col"],[/^K[o]l\D*/i,      "Col"],            //Kol.
+  [/^1\s*Th\D*/i,    "1Thess"],[/^1\s*Te\D*/i,    "1Thess"],    //1Tes.
+  [/^2\s*Th\D*/i,    "2Thess"],[/^2\s*Te\D*/i,    "2Thess"],    //2Tes.
+  [/^1\s*T[im]\D*/i,  "1Tim"],                                  //1Tim.
+  [/^2\s*T[im]\D*/i,  "2Tim"],                                  //2Tim.
+  [/^Tit\D*/i,      "Titus"],                                   //Tit.
+  [/^Ph\w*m\D*/i,    "Phlm"],[/^Fl\w*m\D*/i,    "Phlm"],[/^Fi[l]\w*e\D*/i,    "Phlm"],         //Flm.
+  [/^Heb\D*/i,      "Heb"],[/^Ibr\D*/i,      "Heb"],            //Ibr.
+  [/^J[mas]\D*/i,    "Jas"],[/^Y?ak\D*/i,    "Jas"],          //Yak.
+  [/^1\s*P\D*/i,    "1Pet"],                                    //1Pet.
+  [/^2\s*P\D*/i,    "2Pet"],                                    //2Pet.
+  [/^1\s*J\D*/i,    "1John"],[/^1\s*Yo?[ha]\D*/i,    "1John"],        //1Yoh.
+  [/^2\s*J\D*/i,    "2John"],[/^2\s*Yo?[ha]\D*/i,    "2John"],        //2Yoh.
+  [/^3\s*J\D*/i,    "3John"],[/^3\s*Yo?[ha]\D*/i,    "3John"],        //3Yoh.
+  [/^Jud\D*/i,      "Jude"],[/^Yu\D*/i,      "Jude"],           //Yud.
+  [/^R[ev]\D*/i,    "Rev"],[/^W[ah]\D*/i,    "Rev"], [/^Why\D*/i,    "Rev"]             //Wah.
+];
+
+function toSemiOsis(ref){
+  var parts = ref.replace(/^\s+|\s+$/g, '').replace(/\u2013|\u2014/g, '-').split(/\s*-+\s*/);
+  //"[regEx for deteksiMasukanNamaKitab, targetOsis]"
+  var formatOsis;
+  var kitabAwal,
+      pasalAwal,
+      ayatAwal,
+      kitabAkhir,
+      pasalAkhir,
+      ayatAkhir;
+  
+  //                                                         e[0]                         e[1]
+  Array.prototype.forEach.call(FilterKitab,function(e){//"[regEx for deteksiMasukanNamaKitab, targetOsis]"
+    if(e[0].test(parts[0])){
+      kitabAwal = e[1];
+      parts[0] = parts[0].replace(e[0], '');
+      var pasal_ayat = parts[0].match(/(\d+)(?:\D+(\d+)(?:\s*,\s*(\d+))?)?/);
+      if(pasal_ayat){
+        pasalAwal = parseInt(pasal_ayat[1], 10);
+        if(pasal_ayat[2])
+          ayatAwal = parseInt(pasal_ayat[2], 10);
+        if(pasal_ayat[3]){
+          kitabAkhir = kitabAwal;
+          pasalAkhir = pasalAwal;
+          ayatAkhir = parseInt(pasal_ayat[3], 10);
+          if(ayatAkhir != ayatAwal+1)
+            ayatAkhir = null;
+        }
+      }
+      
+      return false;
+    }
+    return true;
+  });
+  if(!kitabAwal)
+    return '';
+  
+  formatOsis = kitabAwal;
+  
+  if(pasalAwal) formatOsis += '.' + pasalAwal;
+  
+  if(ayatAwal) formatOsis += '.' + ayatAwal;
+  
+  if(parts[1]){
+     Array.prototype.forEach.call(FilterKitab,function(e){
+      if(e[0].test(parts[1])){
+        kitabAkhir = e[1];
+        parts[1] = parts[1].replace(e[0], '');
+        return false;
+      }
+      return true;
+    });
+    var pasal_ayat = parts[1].match(/(\d+)(?:\D+(\d+))?/);
+    if(pasal_ayat){
+      if(!pasal_ayat[2] && !kitabAkhir){
+        if(ayatAwal){
+          pasalAkhir = pasalAwal;
+          ayatAkhir = parseInt(pasal_ayat[1], 10);
+        }
+        else if(pasalAwal){
+          pasalAkhir = parseInt(pasal_ayat[1], 10);
+        }
+      }
+      else {
+        pasalAkhir = parseInt(pasal_ayat[1], 10);
+        if(pasal_ayat[2])
+          ayatAkhir = parseInt(pasal_ayat[2], 10);
+      }
+    }
+    
+    if(!kitabAkhir)
+      kitabAkhir = kitabAwal;
+  }
+  if(kitabAkhir){
+    formatOsis += '-' + kitabAkhir;
+    if(pasalAkhir)
+      formatOsis += '.' + pasalAkhir;
+    if(ayatAkhir)
+      formatOsis += '.' + ayatAkhir;
+  }
+  return formatOsis;
+};
+
+
+
+//KITAB Pasal:Ayat k_p_a
+var PraParser = function (k_p_a) {
+    var Alkitab = {
+        kitab: null,
+        pasal: null,
+        ayat: []
+    };
+    var k_p_a = k_p_a.replace(/^\s+|\s+$|(\s)+/g, "$1");
+    var k_p_a = k_p_a.split(" ");
+  if(k_p_a.length>1){
+    //console.log(k_p_a.trim()) 
+    // k_p_a = "kitab pasal:ayat1[[-],]ayat2"
+    // ambil sebelah kanan dari hitungan 0 sampai batas spasi didapat "kitab"
+    //Alkitab.kitab = k_p_a.substring(0, k_p_a.lastIndexOf(" "));//"{kitab} pasal:ayat1[[-],]ayat2"
+    Alkitab.kitab = k_p_a[0];//"{kitab} pasal:ayat1[[-],]ayat2"
+    //Alkitab.kitab =  {kitab} 
+    //
+    //proses berikut ambil sebelah kanan dihitung mulai dari spasi + 1 sampai akhir didapat "pasal:ayat1[[-],]ayat2"
+    //var k_p_a = k_p_a.substring(k_p_a.lastIndexOf(" ") + 1);////k_p_a = "pasal:ayat1[[-],]ayat2"
+    var kpa = k_p_a[1];////k_p_a = "pasal:ayat1[[-],]ayat2"
+    //
+    var KPA = kpa.split(":");  //k_p_a dipisahkan pada batas tanda ":"  menjadi "pasal" dan "ayat1[[-],]ayat2"
+    //KPA bebentuk array KPA[0]="pasal" dan KPA[1]= "ayat1[[-],]ayat2"
+    
+    switch (KPA.length) { 
+        
+        case 2:
+           
+            Alkitab.pasal = KPA[0];
+            var osisName = toSemiOsis(Alkitab.kitab)
+            var pasalMin = 1;
+            var pasalMax = BCV[osisName].length
+            
+            var _ayat = KPA[1].split(/\-|\,/); //KPA[1] dipisahkan lagi dengan tanda "-" atau tanda "," 
+            var ayat1, ayat2;
+            var ayatMin= 1;
+            var ayatMax = BCV[osisName][Alkitab.pasal-1]
+            //console.log(ayatMax)
+
+            if(parseInt(_ayat[0])<ayatMin){
+              ayat1 = ayatMin
+            }
+            else{
+              ayat1 = parseInt(_ayat[0])
+            }
+            
+            if(parseInt(_ayat[0])>ayatMax){
+              ayat1=ayatMax
+            }
+            if(parseInt(_ayat[1])<ayatMin){
+              ayat2=ayatMin
+            }
+            else{ayat2=parseInt(_ayat[1])}
+            if(parseInt(_ayat[1])>ayatMax){
+              ayat2=ayatMax
+            }
+            console.log("ayat1 ",ayat1)
+            console.log("ayat2 ",ayat2)
+            if(ayat1>ayat2){
+              var temp=ayat1;
+              ayat1 = ayat2
+              ayat2 = temp
+            }
+            
+            // e.g. 1-10
+            if (KPA[1].indexOf("-") !== -1) {
+              for (var i = ayat1; i <= ayat2; ++i) {
+                Alkitab.ayat.push(i.toString());
+              }
+            } 
+            else {
+              if(_ayat>BCV[osisName][Alkitab.pasal-1])_ayat=BCV[osisName][Alkitab.pasal-1]
+               Alkitab.ayat = _ayat;
+            }
+            
+            break;
+        case 1:
+            Alkitab.pasal = KPA[0];
+            var osisName = toSemiOsis(Alkitab.kitab)
+            var ayatAwal = 1;
+            var ayatAkhir = BCV[osisName][Alkitab.pasal-1]
+            for (var i = ayatAwal; i <= ayatAkhir; ++i) {
+                Alkitab.ayat.push(i.toString());
+            }
+            
+            break;
+            
+        default:
+            return null;
+    }
+    
+  }
+
+  else if(k_p_a.length===1){
+    var dummy = " 1";
+    alkitabdummy="";
+    Alkitab.kitab = k_p_a;
+    alkitabdummy = Alkitab.kitab;
+    alkitabdummy += dummy
+    var osisName = toSemiOsis(alkitabdummy)
+    osisName = osisName.replace(".1","")
+    var pasalLength = BCV[osisName].length;
+    var ayatLength;
+    for(var pas = 0; pas<pasalLength; pas++){
+      Alkitab.pasal = pas+1;
+      ayatLength = BCV[osisName][Alkitab.pasal-1]
+      for(var ayt = 0; ayt<ayatLength; ayt++){
+          Alkitab.ayat.push((ayt+1).toString())
+        }
+    }
+  }
+  return Alkitab;
+};
